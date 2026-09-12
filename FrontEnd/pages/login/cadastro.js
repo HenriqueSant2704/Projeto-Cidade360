39b5ea9a8793e1c9d51f7b16dad6cf240e9f0169
Miguel Santana <henriquesantanamiguel@gmail.com>
2026-09-12 13:47:19 -0300

    Ajustes e correção em todas estrutura

--- FrontEnd/pages/login/cadastro.js
+++ FrontEnd/pages/login/cadastro.js
@@ -537,7 +537,17 @@ function adicionarEventosMostrarSenhaCadastro() {
 
             if (!input) return;
 
-            input.type = input.type === "password" ? "text" : "password";
+            const senhaVisivel = input.type === "text";
+
+            if (senhaVisivel) {
+                input.type = "password";
+                botao.src = "../../../assets/icons/login/olho.png";
+                botao.alt = "Mostrar senha";
+            } else {
+                input.type = "text";
+                botao.src = "../../../assets/icons/login/olho-aberto.png";
+                botao.alt = "Ocultar senha";
+            }
         });
     });
 }
